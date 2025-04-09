0a9c704bf681718954758403a9dadda57914047e
MahfujuRahman <akashajmain0@gmail.com>
2025-04-09 11:23:34 +0600

    before modify

---
+++ app/Http/Controllers/Customer/Models/CustomerNextContactDate.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Customer\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CustomerNextContactDate extends Model
+{
+    use HasFactory;
+
+    public function customer() {
+        return $this->belongsTo(Customer::class, 'customer_id');
+    }
+    
+}
