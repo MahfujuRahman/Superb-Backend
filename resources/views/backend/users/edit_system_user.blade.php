d773bd3ba06d7dbe2fe1a7dcad0e1136288872da
MahfujuRahman <akashajmain0@gmail.com>
2025-04-10 16:07:42 +0600

    Add back navigation button to system user create and edit forms for improved user experience

--- resources/views/backend/users/edit_system_user.blade.php
+++ resources/views/backend/users/edit_system_user.blade.php
@@ -23,7 +23,13 @@
         <div class="col-lg-12 col-xl-12">
             <div class="card">
                 <div class="card-body">
-                    <h4 class="card-title mb-3">System User Update Form</h4>
+                  
+                    <div class="d-flex justify-content-between align-items-center mb-3">
+                        <h4 class="card-title mb-3">System User Update Form</h4>
+                        <a href="{{ route('ViewAllSystemUsers')}}" class="btn btn-secondary">
+                            <i class="fas fa-arrow-left"></i>
+                        </a>
+                    </div>
 
                     <form class="needs-validation" method="POST" action="{{url('update/system/user')}}" enctype="multipart/form-data">
                         @csrf
